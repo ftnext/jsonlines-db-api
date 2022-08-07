09f9b6867655618c028e58bafd2321f166230c7c
ftnext <takuyafjp+develop@gmail.com>
2022-08-07 23:47:50 +0900

    [docs] Enable to be verbose

--- examples/jsonlfile.py
+++ examples/jsonlfile.py
@@ -1,7 +1,19 @@
 """A simple example showing the JSON Lines adapter."""
+import argparse
+import logging
+
 from shillelagh.backends.apsw.db import connect
 
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", "-v", action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
     connection = connect(":memory:")
     cursor = connection.cursor()
 
